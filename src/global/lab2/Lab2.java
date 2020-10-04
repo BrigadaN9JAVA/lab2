1458bc402be9563651fd6fdbc3d042269be9cfa7
MrZend <vlad04102002@gmail.com>
2020-10-04 23:16:31 +0300

    Commenting

--- src/global/lab2/Lab2.java
+++ src/global/lab2/Lab2.java
@@ -2,8 +2,11 @@ package global.lab2;
 
 public class Lab2 {
     public static void main(String[] args){
-        Calculator.printResultT25(2.98, 5.55, 0.045, 0.129);
-        Calculator.printResultT26(1.27, 10.99, 4, -25.32);
-        Calculator.printResultT27(0.478, -1.26, 2.68, 18.24);
+        Calculator.printResultT25(2.98, 5.55, 0.045, 0.129);    // 25th task
+
+        Calculator.printResultT26(1.27, 10.99, 4, -25.32);      // 26th task
+
+        Calculator.printResultT27(0.478, -1.26, 2.68, 18.24);   // 27th task
+
     }
 }
